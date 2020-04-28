6148e80272aafd45045d694e4ea9ace381017082
James Woodley <james@lightestnight.com>
2020-04-28 17:01:30 +0100

    Add ExtendsServiceCollection

---
+++ MediatRCore/ExtendsServiceCollection.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace LightestNight.System.Command.MediatR
+{
+    public static class ExtendsServiceCollection
+    {
+        public static IServiceCollection AddCommandValidation(this IServiceCollection services)
+            => services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehaviour<,>));
+    }
+}
