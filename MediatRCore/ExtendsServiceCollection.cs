6398c384d3ba852795144e3839f9b53312e8c6fb
James Woodley <james@lightestnight.com>
2020-04-30 10:23:48 +0100

    Add the ability to pass through assemblies to AddCommandValidators and therefore cut down on a registration

--- MediatRCore/ExtendsServiceCollection.cs
+++ MediatRCore/ExtendsServiceCollection.cs
@@ -1,11 +1,18 @@
-﻿using MediatR;
+﻿using System.Linq;
+using System.Reflection;
+using MediatR;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace LightestNight.System.Command.MediatR
 {
     public static class ExtendsServiceCollection
     {
-        public static IServiceCollection AddCommandValidation(this IServiceCollection services)
-            => services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehaviour<,>));
+        public static IServiceCollection AddCommandValidation(this IServiceCollection services, params Assembly[] validatorAssemblies)
+        {
+            if (validatorAssemblies.Any())
+                services.AddCommandValidators(validatorAssemblies);
+            
+            return services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehaviour<,>));
+        }
     }
 }
