b8728f48b9b07faff609bfd9df89cb77d910936a
James Woodley <james@lightestnight.com>
2020-06-15 16:18:06 +0100

    Add CommandDecorators
      * Add the injection of CommandDecorators
      * Add CI/CD

--- MediatRCore/ExtendsServiceCollection.cs
+++ MediatRCore/ExtendsServiceCollection.cs
@@ -1,6 +1,8 @@
 ﻿using System.Linq;
 using System.Reflection;
+using LightestNight.System.ServiceResolution;
 using MediatR;
+using MediatR.Pipeline;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace LightestNight.System.Command.MediatR
@@ -13,6 +15,41 @@ namespace LightestNight.System.Command.MediatR
                 services.AddCommandValidators(validatorAssemblies);
             
             return services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehaviour<,>));
+        }
+
+        public static IServiceCollection AddCommandDecorators(this IServiceCollection services,
+            params Assembly[] decoratorAssemblies)
+        {
+            AssemblyScanning.RegisterServices(services, decoratorAssemblies, new[]
+            {
+                new ConcreteRegistration
+                {
+                    InterfaceType = typeof(INotificationHandler<>),
+                    AddIfAlreadyExists = true
+                },
+                new ConcreteRegistration
+                {
+                    InterfaceType = typeof(IRequestPreProcessor<>),
+                    AddIfAlreadyExists = true
+                },
+                new ConcreteRegistration
+                {
+                    InterfaceType = typeof(IRequestPostProcessor<,>),
+                    AddIfAlreadyExists = true
+                },
+                new ConcreteRegistration
+                {
+                    InterfaceType = typeof(IRequestExceptionHandler<,,>),
+                    AddIfAlreadyExists = true
+                },
+                new ConcreteRegistration
+                {
+                    InterfaceType = typeof(IRequestExceptionAction<,>),
+                    AddIfAlreadyExists = true
+                }
+            });
+
+            return services;
         }
     }
 }
--- README.md
+++ README.md
@@ -2,3 +2,36 @@
 ## Command > MediatR
 
 Elements focussed on the Command side of the CQRS pattern specialised to the [MediatR library](https://github.com/jbogard/MediatR "MediatR")
+
+### Build Status
+![](https://github.com/lightest-night/system.api.rest.hypermedia/workflows/CI/badge.svg)
+![](https://github.com/lightest-night/system.api.rest.hypermedia/workflows/Release/badge.svg)
+
+#### How To Use
+#### Registration
+##### Validation
+* Asp.Net Standard/Core Dependency Injection
+  * Use the provided `services.AddCommandValidation(params Assembly[] validatorAssemblies)` method
+
+* Other containers
+  * Register all instances of `IPipelineBehavior<,>` in your container
+  
+##### Decorators
+* Asp.Net Standard/Core Dependency Injection
+  * Use the provided `serviced.AddCommandDecorators(params Assembly[] decoratorAssemblies)` method
+  
+* Other containers
+  * Register all instances of `INotificationHandler<>` in your container
+  * Register all instances of `IRequestPreProcessor<>` in your container
+  * Register all instances of `IRequestPostProcessor<,>` in your container
+  * Register all instances of `IRequestExceptionHandler<,,>` in your container
+  * Register all instances of `IRequestExceptionAction<,>` in your container
+  
+#### Usage
+##### Validators
+* Create any Validation classes and inherit from `ICommandValidator<>`
+
+##### Decorators
+* Create any Decorator classes and inherit from `INotificationHandler<TNotification`, `IRequestPreProcessor<TRequest>`, `IRequestPostProcessor<TRequest, TResponse>`, `IRequestExceptionHandler<TRequest, TResponse, TException>`, or `IRequestExceptionAction<TRequest, TException>`
+
+The validation and/or decorator classes will be automatically injected into the pipeline
