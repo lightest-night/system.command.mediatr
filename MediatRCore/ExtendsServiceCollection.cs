dcd4ef254e555d3ffebeba0d7fd32110a4a1b387
James Woodley <james@lightestnight.com>
2020-06-15 21:23:55 +0100

    Remove CommandDecorators, seems MediatR injects them automatically

--- MediatRCore/ExtendsServiceCollection.cs
+++ MediatRCore/ExtendsServiceCollection.cs
@@ -16,40 +16,5 @@ namespace LightestNight.System.Command.MediatR
             
             return services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehaviour<,>));
         }
-
-        public static IServiceCollection AddCommandDecorators(this IServiceCollection services,
-            params Assembly[] decoratorAssemblies)
-        {
-            AssemblyScanning.RegisterServices(services, decoratorAssemblies, new[]
-            {
-                new ConcreteRegistration
-                {
-                    InterfaceType = typeof(INotificationHandler<>),
-                    AddIfAlreadyExists = true
-                },
-                new ConcreteRegistration
-                {
-                    InterfaceType = typeof(IRequestPreProcessor<>),
-                    AddIfAlreadyExists = true
-                },
-                new ConcreteRegistration
-                {
-                    InterfaceType = typeof(IRequestPostProcessor<,>),
-                    AddIfAlreadyExists = true
-                },
-                new ConcreteRegistration
-                {
-                    InterfaceType = typeof(IRequestExceptionHandler<,,>),
-                    AddIfAlreadyExists = true
-                },
-                new ConcreteRegistration
-                {
-                    InterfaceType = typeof(IRequestExceptionAction<,>),
-                    AddIfAlreadyExists = true
-                }
-            });
-
-            return services;
-        }
     }
 }
